a066ad153e4e0af55f9e364704609dd96d54ad6c
Omkar Patil <omkarpatil2510@gmail.com>
2016-12-05 19:31:14 +0530

    Initial commit

---
+++ lib/google/places.rb
@@ -0,0 +1,8 @@
+require "google/places/version"
+require "httparty"
+
+module Google
+  module Places
+    # Your code goes here...
+  end
+end
---
+++ lib/google/places/version.rb
@@ -0,0 +1,5 @@
+module Google
+  module Places
+    VERSION = "0.0.1"
+  end
+end
